c76a9323a6d18d4024ead0c1f1c9e3df0d8ba2d1
SmrutiChoudhary23 <choudharismruti23@gmail.com>
2026-09-20 17:08:25 +0530

    Add destination exploration section

--- public/script.js
+++ public/script.js
@@ -13,3 +13,35 @@ cards.forEach((card) =>
 {
     observer.observe(card);
 });
+
+/*Explore Page*/
+const districts = document.querySelectorAll('#districtList li')
+const destinations = document.querySelectorAll('.districtDestination')
+districts.forEach(function(district) 
+{
+    district.addEventListener('click', function()
+    {   
+        const districtName = district.textContent.toLowerCase();
+
+        const selectedDestination = document.querySelector(`.districtDestination[data-district="${districtName}"]`);
+
+        if(selectedDestination.classList.contains('active'))
+        {
+            selectedDestination.classList.remove('active')
+            district.classList.remove('active')
+        }
+        else
+        {
+            destinations.forEach(function(destination)
+            {
+                destination.classList.remove('active')
+            });
+            districts.forEach(function(districtItem)
+            {
+                districtItem.classList.remove("active");
+            });
+            selectedDestination.classList.add('active');
+            district.classList.add("active");
+        }
+    });
+});
